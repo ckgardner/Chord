6ad2a8b1a9057d1c3c16875bdaabc90aa584f312
cade.gardner <d00299297@dmail.dixie.edu>
2020-02-29 10:15:43 -0700

    server update

---
+++ server.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+)
+
+func server(myNode *Node) {
+	location := myNode.Port
+	rpc.Register(myNode)
+	rpc.HandleHTTP()
+	listener, err := net.Listen("tcp", location)
+	if err != nil {
+		log.Fatal("Error thrown while listening: ", err)
+	}
+	fmt.Printf("Listening %v\n", location)
+
+	go func() {
+		if err := http.Serve(listener, nil); err != nil {
+			log.Fatalf("Serving: %v", err)
+		}
+	}()
+	fmt.Println("Server is on")
+}
+
+func join(myNode *Node, location string) error {
+	var nothing Nothing
+	if err := call(location, "Node.Ping", nothing, &nothing); err != nil {
+		log.Printf("Connection not working: %v\n", err)
+		return err
+	}
+	myNode.Successors[0] = location
+	return nil
+}
+
+func call(address string, method string, request interface{}, reply interface{}) error {
+	client, err := rpc.DialHTTP("tcp", address)
+	if err != nil {
+		log.Printf("rpc.DialHTTP: %v", err)
+		return err
+	}
+
+	defer client.Close()
+
+	if err = client.Call(method, request, reply); err != nil {
+		log.Printf("client.Call %s: %v", method, err)
+		return err
+	}
+
+	return nil
+}
